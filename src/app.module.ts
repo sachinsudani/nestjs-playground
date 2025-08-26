00c8ec8a0acd9bed9f44dbcf331344b912b83256
Sachin Sudani <sachin@xenett.com>
2025-08-26 19:12:22 +0530

    feat: add product module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [UserModule],
+    imports: [UserModule, ProductModule],
     controllers: [AppController],
     providers: [AppService]
 })
