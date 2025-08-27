c681b35cd4d70d6404e790fc065636c381e9e5d3
Sachin Sudani <sachin@xenett.com>
2025-08-27 18:13:47 +0530

    feat: enhance product service and controller with CRUD operations

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
@@ -7,7 +7,32 @@ export class ProductController {
     constructor(private readonly productService: ProductService) { }
 
     @Get()
-    getProducts() {
-        return this.productService.getProducts();
+    getAll() {
+        return this.productService.getAllProducts();
+    }
+
+    @Get(':id')
+    getById(@Param('id') id: string) {
+        return this.productService.getProductById(+id);
+    }
+
+    @Post()
+    create(@Body() product: { name: string; price: number }) {
+        return this.productService.createProduct(product);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() product: { name: string; price: number }) {
+        return this.productService.updateProduct(+id, product);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') id: string, @Body() product: Partial<{ name: string; price: number }>) {
+        return this.productService.patchProduct(+id, product);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.productService.deleteProduct(+id);
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class ProductService {
-    getProducts(): string[] {
-        return ['Mobile', 'Laptop', 'Tablet'];
+    private products: { id: number; name: string; price: number }[] = [{
+        id: 1,
+        name: "Apple iPhone 14",
+        price: 59000
+    }, {
+        id: 2,
+        name: "Apple MacBook Pro",
+        price: 149900
+    }];
+
+    getAllProducts() {
+        return this.products;
+    }
+
+    getProductById(id: number) {
+        const product = this.products.find(product => product.id === id);
+
+        if (!product) throw new NotFoundException('Product not found');
+
+        return product;
+    }
+
+    createProduct(product: { name: string; price: number }) {
+        const newProduct = {
+            id: this.products.length + 1,
+            ...product
+        };
+        this.products.push(newProduct);
+        return newProduct;
+    }
+
+    updateProduct(id: number, product: { name: string; price: number }) {
+        const index = this.products.findIndex(product => product.id === id);
+
+        if (index === -1) throw new NotFoundException('Product not found');
+
+        this.products[index] = {
+            id,
+            ...product
+        };
+
+        return this.products[index];
+    }
+
+    patchProduct(id: number, data: Partial<{ name: string; price: number }>) {
+        const product = this.getProductById(id);
+        Object.assign(product, data);
+        return product;
+    }
+
+    deleteProduct(id: number) {
+        const index = this.products.findIndex(product => product.id === id);
+
+        if (index === -1) throw new NotFoundException('Product not found');
+
+        const deletedProduct = this.products.splice(index, 1);
+        return { message: "Product deleted successfully", product: deletedProduct[0] }
     }
 }
